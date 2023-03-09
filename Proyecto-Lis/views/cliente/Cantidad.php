8e3ca7c3bfbc4ac8adccb529bf440b310329c017
AbigaailAcuna <acuaabigail@yahoo.com>
2023-03-09 12:57:34 -0600

    modificando cantidades carrito

--- Proyecto-Lis/views/cliente/Cantidad.php
+++ Proyecto-Lis/views/cliente/Cantidad.php
@@ -3,27 +3,32 @@ session_start();
 
 //vemos qué acción hay que ejecutar
 if(isset($_SESSION['carrito'])){
-$arreglo=$_SESSION['carrito'];   
-//hay una acción en la url
-if(isset($_GET["a"])){
-    if($_GET["a"]=="sumar"){
-        for($i=0;$i<count($arreglo);$i++){
-            $numero=$i;
-            $arreglo[$numero]['Cantidad']=$arreglo[$numero]['Cantidad']+1;
-            $_SESSION['carrito']=$arreglo;
-        }
-        header('Location:?c=Principal&a=carrito');
+    $arreglo=$_SESSION['carrito'];   
+    //hay una acción en la url
+    if(isset($_GET["a"])){
+        if($_GET["a"]=="sumar"){
+            for($i=0;$i<count($arreglo);$i++){
+                if($arreglo[$i]['Id']==$_GET['id']){
+                $numero=$i;
+                $arreglo[$numero]['Cantidad']=$arreglo[$numero]['Cantidad']+1;
+                $_SESSION['carrito']=$arreglo;
+            }
+            header('Location:?c=Principal&a=carrito');
+            }
+   
+         }
+         if($_GET["a"]=="restar"){
+            for($i=0;$i<count($arreglo);$i++){
+                if($arreglo[$i]['Id']==$_GET['id']){
+                $numero=$i;
+                $arreglo[$numero]['Cantidad']=$arreglo[$numero]['Cantidad']-1;
+                $_SESSION['carrito']=$arreglo;
+            }
+            header('Location:?c=Principal&a=carrito');
+            }
+   
+         }
     }
+}
 
-    //borrar
-    if($_GET["a"]=="restar"){
-        for($i=0;$i<count($arreglo);$i++){
-            $numero=$i;
-            $arreglo[$numero]['Cantidad']=$arreglo[$numero]['Cantidad']-1;
-            $_SESSION['carrito']=$arreglo;
-        }
-        header('Location:?c=Principal&a=carrito');
-    }
-}
-}
 ?>
