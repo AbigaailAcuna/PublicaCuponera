604d7520b20ef329bd8f6c6c606683e1c1c27acc
Ortega02 <89029520+Ortega02@users.noreply.github.com>
2023-04-05 10:24:22 -0600

    Add files via upload

---
+++ Proyecto-Lis/views/cliente/Registro.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registro de usuario</title>
+  <link href="http://localhost/Proyecto-Lis/recursos/css/log.css" rel="stylesheet" />
+</head>
+<body>
+  <div class="vid-container">
+    <form method="POST" action="?c=Usuario&a=registrar">
+      <div class="box">
+        <h1>Registro</h1>
+        <input type="text" name="nombres" id="nombres" placeholder="Nombres" required />
+        <input type="text" name="apellidos" id="apellidos" placeholder="Apellidos" required/>
+        <input type="text" name="telefono" id="telefono" placeholder="Teléfono" pattern="[0-9_-]{8,9}" required/>
+        <input type="text" name="direccion" id="direccion" placeholder="Dirección" required/>
+        <input type="text" name="dui" id="dui" placeholder="Dui" pattern="[0-9_-]{9,10}" required/>
+        <input type="email" name="correo" id="correo" placeholder="Correo" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required/>
+        <input type="password" name="password" id="password" placeholder="Contraseña" required/>
+        <button name="signup" id="signup" >Registrarse</button>
+        <p>¿Ya posees una cuenta?</p> 
+        <p><a href="?c=Principal&a=inicio">Iniciar Sesión</a><p>
+        <?php 
+        //mensaje de error ?>
+      </div>
+    </form>
+  </div>
+</body>
+<footer>
+<!---?php include('../layouts/footer.php'); ?--->
+</footer>
+</html>
---
+++ Proyecto-Lis/views/cliente/ValidarCorreo.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La cuponera</title>
+    <link href="http://localhost/Proyecto-Lis/recursos/css/log.css" rel="stylesheet" />
+</head>
+<body>
+  <form action="?c=Usuario&a=validarRegistro" method="POST" >
+    <div class="vid-container">
+      <div class="box">
+        <h1>Validar Token</h1>
+        
+        <input type="email" name="correo" id="correo" placeholder="Correo" required/>
+        <input type="text" name="token" id="token" placeholder="Token" required/>
+        <button name="validar" id="validar">Validar</button>
+
+      </div>
+    </div>
+  </form>
+</body>
+<footer>
+<?php include('../layouts/footer.php'); ?>
+</footer>
+</html>
