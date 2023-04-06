be18d6675dcd7489abd57593e18508cee88012f0
lupitasgm <93928511+lupitasgm@users.noreply.github.com>
2023-04-06 01:05:32 -0600

    Update InicioSesion.php

--- Proyecto-Lis/views/cliente/InicioSesion.php
+++ Proyecto-Lis/views/cliente/InicioSesion.php
@@ -1,33 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio de Sesión</title>
-    <link href="http://localhost/Proyecto-Lis/recursos/css/log.css" rel="stylesheet" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inicio de Sesión</title>
+  <link href="http://localhost/Proyecto-Lis/recursos/css/log.css" rel="stylesheet" />
 </head>
 <body>
-  <form action="?c=Usuario&a=validar" method="POST" >
-    <div class="vid-container">
-      <div class="box">
+  <div class="login-page">
+    <div class="form">
+      <form class="login-form" action="?c=Usuario&a=validar" method="POST">
         <h1>Inicio de Sesión</h1>
-        
-        <input type="email" name="correo" id="correo" placeholder="Correo" required/>
-        <input type="password" name="clave" id="clave" placeholder="Contraseña" required/>
+        <input type="email" name="correo" id="correo" placeholder="Correo"/>
+        <input type="password" name="clave" id="clave" placeholder="Contraseña"/>
         <button name="login" id="login">Iniciar Sesión</button>
         
-        <p>¿No posees una cuenta?</p>
-        <p><a href="?c=Principal&a=registro">Registrarse</a><p>
-        <p>¿Quieres cambiar tu Contraseña?</p>
-        <p><a href="?c=Principal&a=registro">Cambiar contraseña</a><p>
+        <?php
+        if (isset($_SESSION['error'])) {
+            echo '<div class="messageerror">';
+            echo '' . $_SESSION['error'] . '';
+            echo '</div>';
+            unset($_SESSION['error']);
+        }
+        ?>
         
-        <?php //include_once('../../controllers/userInicioSesion.php'); ?>
-      </div>
+        <p class="message"><a href="?c=Principal&a=registro">¿No posees una cuenta?</a></p>
+      </form>
     </div>
-  </form>
+  </div>
 </body>
-<footer>
-<?php include('views/layouts/footer.php'); ?>
-</footer>
 </html>
