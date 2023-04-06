c27c66ac563f99ffa538d72c252f16f143a89f47
lupitasgm <93928511+lupitasgm@users.noreply.github.com>
2023-04-06 01:08:04 -0600

    Update ValidarCorreo.php

--- Proyecto-Lis/views/cliente/ValidarCorreo.php
+++ Proyecto-Lis/views/cliente/ValidarCorreo.php
@@ -1,27 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>La cuponera</title>
-    <link href="http://localhost/Proyecto-Lis/recursos/css/log.css" rel="stylesheet" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Validar Correo</title>
+  <link href="http://localhost/Proyecto-Lis/recursos/css/log.css" rel="stylesheet" />
 </head>
 <body>
-  <form action="?c=Usuario&a=validarRegistro" method="POST" >
-    <div class="vid-container">
-      <div class="box">
+  <div class="login-page">
+    <div class="form">
+      <form class="login-form" action="?c=Usuario&a=validarRegistro" method="POST">
         <h1>Validar Token</h1>
-        
         <input type="email" name="correo" id="correo" placeholder="Correo" required/>
         <input type="text" name="token" id="token" placeholder="Token" required/>
         <button name="validar" id="validar">Validar</button>
+        
+        <?php
+        if (isset($_SESSION['error'])) {
+          echo '<div class="messageerror"><i>&#9888;</i>';
+          echo '' . $_SESSION['error'] . '';
+          echo '</div>';
+          unset($_SESSION['error']);
+        }
+        if (isset($_SESSION['message'])) {
+          echo '<div class="messagee"><i>&#9888;</i>';
+          echo '' . $_SESSION['message'] . '';
+          echo '</div>';
+          unset($_SESSION['message']);
+        }
+        ?>
 
-      </div>
-    </div>
-  </form>
+        <p class="message"><a href="?c=Principal&a=inicio"> Iniciar Sesión</a></p>
+      </form>
+  </div>
+</div>
 </body>
-<footer>
-<?php include('../layouts/footer.php'); ?>
-</footer>
 </html>
