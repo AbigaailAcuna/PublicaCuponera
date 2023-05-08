5ac27f981ce1c783a525d9145d31c287e5db771f
AbigaailAcuna <87994503+AbigaailAcuna@users.noreply.github.com>
2023-05-08 16:49:08 -0600

    Delete Proyecto-Lis directory

--- Proyecto-Lis/views/layouts/footer.php
+++
@@ -1,7 +0,0 @@
-<link href="http://localhost/Proyecto-Lis/recursos/css/header.css" rel="stylesheet" />
-  <!-- Footer-->
-  <footer class="py-5 bg-dark">
-        <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
-        </footer>        
-    </body>
-</html>
